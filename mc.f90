ec1695e105f2cf02339a97a7e68b0b63e99fbcf4
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2018-04-30 00:18:20 -0400

    Create mc.f90

---
+++ mc.f90
@@ -0,0 +1,33 @@
+!-------Monte Carlo evaluation of pi-------
+!By Amol Deshmukh, The City College of New York, 30 April 2018.
+
+program montecarlo
+
+!	integer::N
+!	N=10
+
+	real::a(10000000),b(10000000),c(10000000)
+	real::pi,d
+	integer :: i
+
+	call random_number(a) 
+	call random_number(b)
+	
+	do i=1,10000000
+		c(i)=sqrt(a(i)*a(i)+b(i)*b(i))
+	end do	
+
+	do i=1,10000000
+		if (c(i)>1) then
+			c(i)=0
+		else 
+			c(i)=1	
+		end if
+	end do
+		
+	d=sum(c)
+	pi=d/10000000
+
+	print *,pi*4
+
+end program montecarlo
