f240425c6859f4c3a3fa24c5a823680fe4887859
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2018-05-02 23:54:06 -0400

    Update 1d-HO.py

--- 1d-HO.py
+++ 1d-HO.py
@@ -1,16 +1,21 @@
+# Numerical evaluation of a 1-dimensional quantum harmonic oscillator
+# Example from: arXiv:hep-lat/0506036
+# By Amol Deshmukh, The City College of New York, 2 May 2018.
+
 import numpy as np
 from math import exp
 from scipy import integrate
 
-N=3.5
-i=0
+N=10        #Integration interval
+x_init=0    #Initial position of the particle 
+a=1         #Lattice spacing
 
-def f(x_1,x_2,x_3,x_4):
-	return exp(-(x_1-i)**2-(x_2-x_1)**2-(x_3-x_2)**2-(x_4-x_3)**2-(i-x_4)**2
-		+(1/4)*(-x_1**2-x_2**2-x_3**2-x_4**2-2*i**2))
+def f(x_1,x_2,x_3):
+	return exp(-(1/2)*(x_1-x_init)**2-(1/2)*(x_2-x_1)**2-(1/2)*(x_3-x_2)**2-(1/2)*(x_init-x_3)**2
+		+a*(1/2)*(-x_1**2-x_2**2-x_3**2-2*x_init**2))
 
-a,b=integrate.nquad(f,[[-N,N],[-N,N],[-N,N],[-N,N]])
+a,b=integrate.nquad(f,[[-N,N],[-N,N],[-N,N]])
 
-c=a*(1/np.pi)**(6/2)
+prop=a*(1/(2*np.pi))**(4/2)
 	
-print("Result is {:f} and {:g}".format(c,b))
+print("Numerical value of the propagator is {:f}, with the uncertainty of {:g}".format(prop,b))
