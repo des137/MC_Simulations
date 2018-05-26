f7394288e371326677f0be8fb7debd9b88c1c91f
Amol Deshmukh <34318357+des137@users.noreply.github.com>
2018-05-26 00:06:59 -0400

    Update ising.py

--- ising.py
+++ ising.py
@@ -10,43 +10,42 @@ import matplotlib.animation as animation
 
 # System parameters
 N     = 20		# Number of lattice sites
-N_sim = 10000	# Simulation steps
-beta  = 0.01 	# Temperature
+N_sim = 300000	# Simulation steps
+N_coll= 100	    # Collected steps
+T     = 2.2 	# Temperature
 
-# Total energy of the system
-def energy(state):
-	energy=0
-	for i in range(N-1):
-		for j in range(N-1):
-			energy+=-state[i,j]*(state[i+1,j]+state[i,j+1]) 	
-	for i in range(N-1):						# Periodic boundary conditions
-		energy+=-state[i,N-1]*(state[i+1,N-1]+state[i,0]) 
-	for i in range(N-1):						# Periodic boundary conditions
-		energy+=-state[N-1,i]*(state[N-1,i+1]+state[0,i])
-	energy+=-state[N-1,N-1]*(state[N-1,0]+state[0,N-1])		# Last entry in the matrix
-	return energy	
-
-
-def update(state):
-	for _ in range(N_sim):
-		old_state=state
-		old_ene = energy(old_state)
+# Local energy change of the system after a spin flip
+def diff_energy(state,i,j):
+	return 2*state[i,j]*(state[(i-1)%N,j]+state[i,(j-1)%N]+state[(i+1)%N,j]+state[i,(j+1)%N]) 	
+	
+def update(state,counter_coll):
+	counter=0
+	for _ in range(N_sim):		
 		i = np.random.randint(N)
 		j = np.random.randint(N)
-		state[i,j] = (-1)*state[i,j]
-		new_ene = energy(state)
-		delta_E=new_ene-old_ene
-		if delta_E>0 or np.exp(-beta*delta_E)<np.random.uniform(0,1):
+		delta_E=diff_energy(state,i,j)
+		if delta_E<0 or np.exp(-delta_E/T)>np.random.uniform(0,1):
 			state[i,j] = (-1)*state[i,j]
+		if not counter%N_coll:				
+			counter_coll[counter//N_coll]=(state.sum())
+		counter+=1	
 
 # Intial state
 state = 2*np.random.randint(2, size=(N,N))-1
+
+# Counter of the states
+counter_coll=np.zeros(N_sim//N_coll)
+
 # Monte Carlo updates
-update(state)
+update(state,counter_coll)
 
 # Snapshot of the Ising model
 plt.imshow(state, interpolation='nearest')
 plt.gray()
+
+plt.plot(counter_coll)
+plt.ylim(-N**2,N**2)
+plt.grid(True)
 plt.show()
 
 exit()
