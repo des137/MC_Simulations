66924e9149a3321260b52796ce9c259a85fc71d3
amoldeshmukh137@gmail.com <amoldeshmukh137@gmail.com>
2018-06-07 01:02:01 -0400

    Metropolis implementation in cpp (bugs).

---
+++ temp/ising.cpp
@@ -0,0 +1,81 @@
+/*
+Simulates Ising model using Monte Carlo techniques
+Amol Deshmukh
+*/
+
+#include <iostream>
+#include <ctime>
+#include <math.h>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	// Size of the lattice
+	int N_size = pow(10,3);
+	int Latt[N_size][N_size];
+
+	// Random seed
+	srand(time(NULL));
+
+	// A complicated way of generating a lattice of +1s and -1s
+	for (int i = 0; i < N_size; ++i)
+	{
+		for (int j =0; j < N_size; j++)
+		{
+			double a;
+			a = ((double)rand() / (RAND_MAX));
+			if (a<0.5)
+				{
+					a=-1;
+				}
+			else
+				{
+					a=1;
+				}
+			Latt[i][j]= a;
+		}
+	}	
+
+	// Monte Carlo sweeps
+	int N_sweeps = pow(10,3);
+	for (int i=1; i < N_sweeps; ++i)
+	{	
+		int k,l;
+		int sum[N_sweeps];
+		float temp = 1.1;
+		double local_energy;
+
+		// Choosing a random spin on a lattice
+		k=rand()%N_size;
+		l=rand()%N_size;
+
+		// Local energy 
+		local_energy = 2*Latt[k][l]*
+			(Latt[(k+1)%N_size][l]
+			+Latt[k][(l+1)%N_size]
+			+Latt[(k-1)%N_size][l]
+			+Latt[k][(l-1)%N_size]);
+
+		// Metropolis step
+		if (local_energy<0 || exp(-local_energy/temp)>rand())
+		{
+			Latt[k][l]*=(-1);
+		}
+		else
+		{
+			Latt[k][l]*=(1);	
+		}	
+
+		// Magnetization: (has issues)
+		for(int m=0; m<N_size; m++)
+		{
+			for(int k=0; k<N_size; k++)
+			{
+				sum[i] = sum[i] + Latt[m][k];
+			}
+		}
+		cout<< sum[i] <<endl;	
+	}	
+}
